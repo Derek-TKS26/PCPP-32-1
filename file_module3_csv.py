42b311d60262ab8f70ce3067c0edf5fe918707b7
Derek-TKS26 <cfy86@163.com>
2024-10-23 23:33:48 +0800

    Update file_module3_csv.py

--- file_module3_csv.py
+++ file_module3_csv.py
@@ -12,10 +12,34 @@
 #—————————————————— Reading data from csv document ————————————————————
 import csv
 
-with open('contacts.csv', newline='') as csvfile:
+with open('../../OneDrive/Desktop/contacts.csv', newline='') as csvfile:
     reader = csv.reader(csvfile,delimiter=',')
     # newline='' argument is not absolutely indispensable,
     # but it's good practice when working with CSV files in Python.
 
+    # for row in reader:
+    #     print(','.join(row)) # 用join 函数把列表中的字符元素通过符号连起来
+
+    names = ['Name', 'Phone','age'] #定义表头的不同名字
+    reader = csv.DictReader(csvfile,fieldnames=names) # DirectReader 直接把第一行作为表头，第一个参数是文件的名字而第二个参数则是标头
     for row in reader:
-        print(','.join(row)) # 用join 函数把列表中的字符元素通过符号连起来
+        print(row['Name'],row['Phone'],row['age']) #打印不同表头下的数据
+
+
+#—————————————————— Writing data from csv document ————————————————————
+
+with open('new_contacts.csv','w',newline='') as csv_new:
+    writer = csv.writer(csv_new,delimiter=',',quotechar='?')
+    writer.writerow(names)
+    writer.writerow(['mother', '222-555-101']) #写入数据的行。
+    writer.writerow(['father', '222-555-102'])
+    writer.writerow(['wife', '222-555-103'])
+    writer.writerow(['mother-in-law', '222-555-104'])
+    writer.writerow(['grandmother, grandfather', '222-555-105']) # 如果不用？则默认带有Comma 的项会被特殊引用
+
+    fieldnames = names
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames) # 和上面的DirectReader 一样，DirectWriter 也需要输入两个参数：文件名和表头行
+
+
+
+
